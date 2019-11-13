420ebdfa6d4db79535c5a166187bf5fc6f3b3a00
iamparnab <sanyal.parnab96@gmail.com>
2019-11-14 02:03:50 +0530

    Added Stuff: 	1. Typescript 	2. Sass 	3. HMR

---
+++ src/react/components/App/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.scss";
+export const App = () => {
+  return (
+    <div>
+      <h1>This is TSX</h1>
+      <input placeholder="Enter name please.." />
+    </div>
+  );
+};
