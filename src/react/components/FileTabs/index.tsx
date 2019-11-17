861bf06ae2c66990122691279b097ba52b791612
Sayantan Ghosh <sayantan.ghosh03@gmail.com>
2019-11-17 17:45:26 +0530

    1. Changed the css in the close icon in FileTabs.

--- src/react/components/FileTabs/index.tsx
+++ src/react/components/FileTabs/index.tsx
@@ -64,11 +64,16 @@ export const FileTabs = (props: Props) => {
             <div
               className="close flex-absolute-center"
               onClick={ev => closeHandler(ev, index)}
-              style={{
-                color: FileTabs.tabCloseIconColor
-              }}
             >
-              &times;
+              <svg
+                fill={FileTabs.tabCloseIconColor || "#fff"}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="16px"
+                height="16px"
+              >
+                <path d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z" />
+              </svg>
             </div>
           </div>
         );
